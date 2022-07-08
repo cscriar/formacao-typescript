a87b8526b8747d92fe0d194d3e8c46748e967395
C1310810 Francisca das Chagas dos Santos <francisca.santos@capgemini.com>
2022-07-07 22:42:24 -0300

    Teste

--- ts-react/src/App.tsx
+++ ts-react/src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-    <h1>Olá mundo</h1>
+      <h1>Olá mundo</h1>
+      <p>Breve descrição</p>
     </div>
   );
 }
